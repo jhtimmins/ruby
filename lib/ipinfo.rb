487e49329dbd2b0e3964bebf56b2b321e7993896
James Timmins <jameshtimmins@gmail.com>
2018-10-13 14:41:05 -0700

    Add new project structure.

--- lib/ipinfo.rb
+++ lib/ipinfo.rb
@@ -11,23 +11,49 @@ module IPinfo
 
   class << self
     attr_accessor :access_token
-    attr_writer :http_adapter
 
-    def lookup(ip=nil)
-      response = if @http_adapter
-        Adapter.new(access_token, @http_adapter)
-      else
-        Adapter.new(access_token)
-      end.get(uri_builder(ip))
+    def initialize(access_token=nil, settings={})
+      @access_token = access_token
+      @http_client = getHttpClient(settings.fetch("http_client", nil))
+      @cache = getCache(settings.fetch("cache", nil))
+    end
+
+    def getDetails(ip_address=nil)
+      details = requestDetails(ip_address)
+    end
+
+    protected
+    def requestDetails(ip_address=nil)
+      # if not in cache
+      response = @http_client.get(escape_path(ip_address))
 
       raise RateLimitError.new(RATE_LIMIT_MESSAGE) if response.status.eql?(429)
 
       Response.from_faraday(response)
+
+      #save to cache
+
+      #return value from cache
+
     end
+
+    def getHttpClient(http_client=nil)
+
+      if client_name
+        @http_client = Adapter.new(access_token, http_client)
+      else
+        @http_client = Adapter.new(access_token)
+      end
+
+    end
+
+    # def getCache(cache=nil)
+    #
+    # end
 
     private
 
-    def uri_builder(ip)
+    def escape_path(ip)
       ip ? "/#{CGI::escape(ip)}" : '/'
     end
   end
